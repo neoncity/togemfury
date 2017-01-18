89eba0cc2a059df1042f795481da7376b2d239d1
Horia Coman <horia141@gmail.com>
2017-01-18 19:49:41 +0200

    Fix issue with improper package name being searched for.

--- src/togemfury.sh
+++ src/togemfury.sh
@@ -20,20 +20,20 @@ do
     shift # past argument or value
 done
 
-if [[ -z $GEMFURY_USER ]]
+if [[ -z ${GEMFURY_USER} ]]
 then
     echo 'Need the --user argument'
     exit 1
 fi
 
-if [[ -z $GEMFURY_API_KEY ]]
+if [[ -z ${GEMFURY_API_KEY} ]]
 then
     echo 'Need the --api_key argument'
     exit 1
 fi
 
 npm pack
-curl -s -F package=@`ls @neoncity/*.tgz` https://${GEMFURY_API_KEY}@push.fury.io/${GEMFURY_USER}/ > result
+curl -s -F package=@`ls ${GEMFURY_USER}-*.tgz` https://${GEMFURY_API_KEY}@push.fury.io/${GEMFURY_USER}/ > result
 if [ -z "$(grep -e ok result)" ]
 then
     rm result
