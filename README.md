6cb9f84e11cc92af98503c467ae0c3703d999f1e
Horia Coman <horia141@gmail.com>
2017-01-18 19:41:06 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# ToGemfury [![Build Status](https://travis-ci.org/neoncity/togemfury.svg?branch=master)](https://travis-ci.org/neoncity/togemfury) [![Coverage](https://codecov.io/gh/neoncity/togemfury/branch/master/graph/badge.svg)](https://codecov.io/gh/neoncity/togemfury)
+
+Pushes the current NPM package to gemfury.
+
+```
+$(npm bin)/togemfury -u <username> -a <token>
+```
---
+++ src/togemfury.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+while [[ $# -gt 1 ]]
+do
+    key="$1"
+
+    case $key in
+        -u|--user)
+            GEMFURY_USER="$2"
+            shift # past argument
+            ;;
+        -a|--api_key)
+            GEMFURY_API_KEY="$2"
+            shift # past argument
+            ;;
+        *)
+            # unknown option
+            ;;
+    esac
+    shift # past argument or value
+done
+
+if [[ -z $GEMFURY_USER ]]
+then
+    echo 'Need the --user argument'
+    exit 1
+fi
+
+if [[ -z $GEMFURY_API_KEY ]]
+then
+    echo 'Need the --api_key argument'
+    exit 1
+fi
+
+npm pack
+curl -s -F package=@`ls @neoncity/*.tgz` https://${GEMFURY_API_KEY}@push.fury.io/${GEMFURY_USER}/ > result
+if [ -z "$(grep -e ok result)" ]
+then
+    rm result
+    exit 1
+fi
+rm result
